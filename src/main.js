ad4319406d1bd633580106c1d0475a3dfcf63357
Ben Meeder <bmeeder22@gmail.com>
2018-11-19 13:18:14 -0600

    Installed vuex (#11)
    Installed vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import store from './store'
+import App from './App.vue'
+import router from './router'
 import Vuetify from 'vuetify/lib'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify)
+Vue.use(Vuex)
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
