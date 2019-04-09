15b0c5dac57cb7dd46f01162d2b6ff742014887c
Ben Meeder <bmeeder22@gmail.com>
2019-04-09 13:11:30 -0500

    homepage working.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,77 +1,32 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
-      <v-flex>
-        <div style="text-align: center;">
-          <v-btn v-if="!isUserLoggedIn()" round to="/login">Login</v-btn>
-          <v-btn v-if="!isUserLoggedIn()" round to="/register">Register</v-btn>
-          <span v-if="isUserLoggedIn()">Logged in as: {{userEmail}}</span>
-          <v-btn v-if="isUserLoggedIn()" round  to="/account">Account</v-btn>
-          <v-btn v-if="isUserLoggedIn()" round  @click="logout" >Logout</v-btn>
-        </div>
-      </v-flex>
+  <v-img
+      :src="require('../assets/play-cow-patty.jpg')"
+      width="95%"
+  ></v-img>
 
-      <!--<v-flex xs12>-->
-        <!--<v-img-->
-            <!--:src="require('../assets/boo_radley.png')"-->
-            <!--class="my-3"-->
-            <!--contain-->
-            <!--height="100"-->
-        <!--&gt;</v-img>-->
-      <!--</v-flex>-->
-
-      <!--<v-flex xs12>-->
-        <!--<v-img-->
-            <!--:src="require('../assets/play-cow-patty.jpg')"-->
-            <!--class="my-3"-->
-            <!--contain-->
-            <!--height="400"-->
-        <!--&gt;</v-img>-->
-      <!--</v-flex>-->
-
-
-      <v-flex mb-4>
-        <div style="text-align: center;">
-          <h1 v-if="isUserLoggedIn()" class="display-4 font-weight-thin mb-3" >
-            Welcome {{userEmail}} to CowChips4Charity!
-          </h1>
-          <h1 v-else class="display-4 font-weight-thin mb-3" >
-            Welcome to CowChips4Charity!
-          </h1>
-
-          <p class="subheading font-weight-regular">
-            For more information about the Boo Radley Foundation's cause, please visit the foundation's
-            <a
-                v-for="(boo, i) in booRadleyFoundation"
-                :key="i"
-                :href="boo.href"
-                class="subheading mx-3"
-                target="_blank"
-            >
-              {{ boo.text }}
-            </a>
-          </p>
-        </div>
-      </v-flex>
-
-      <v-flex md-center xs12 lg12>
-        <div style="text-align: center;">
-          <v-btn large dark round to="/play">Play</v-btn>
-          <v-btn large dark round to="/donation?full=true">Donate</v-btn>
-        </div>
-      </v-flex>
-
-      <v-flex md-center>
+      <!--<v-flex>-->
         <!--<div style="text-align: center;">-->
-        <div class="center2">
-          <v-btn round to="/about">About</v-btn>
-        </div>
-      </v-flex>
+          <!--<v-btn v-if="!isUserLoggedIn()" round to="/login">Login</v-btn>-->
+          <!--<v-btn v-if="!isUserLoggedIn()" round to="/register">Register</v-btn>-->
+          <!--<span v-if="isUserLoggedIn()">Logged in as: {{userEmail}}</span>-->
+          <!--<v-btn v-if="isUserLoggedIn()" round  to="/account">Account</v-btn>-->
+          <!--<v-btn v-if="isUserLoggedIn()" round  @click="logout" >Logout</v-btn>-->
+        <!--</div>-->
+      <!--</v-flex>-->
 
+      <!--<v-flex md-center xs12>-->
+        <!--<div style="text-align: center;">-->
+          <!--<v-btn large dark round to="/play">Play</v-btn>-->
+          <!--<v-btn large dark round to="/donation?full=true">Donate</v-btn>-->
+        <!--</div>-->
+      <!--</v-flex>-->
 
-    </v-layout>
-  </v-container>
-
+      <!--<v-flex md-center>-->
+        <!--&lt;!&ndash;<div style="text-align: center;">&ndash;&gt;-->
+        <!--<div class="center2">-->
+          <!--<v-btn round to="/about">About</v-btn>-->
+        <!--</div>-->
+      <!--</v-flex>-->
 </template>
 
 <script>
@@ -109,23 +64,10 @@
         return ''
       }
     },
-    methods: {
-      logout() {
-        localStorage.eraseCookie(authTokenName)
-        axios.defaults.headers['Authorization'] = localStorage.getCookie(authTokenName)
-        this.$forceUpdate()
-      },
-      isUserLoggedIn() {
-        return localStorage.isUserLoggedIn()
-      }
-    }
+
   }
 </script>
 
 <style>
-
-  .center2 {
-    text-align: center;
-  }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,74 @@
 <template>
-  <HomePage />
+  <div class="whole">
+    <v-img
+        :src="require('../assets/play-cow-patty.jpg')"
+        width="100%"
+    ></v-img>
+    <div class="button-container">
+
+      <table v-if="isUserLoggedIn()" class="whole">
+        <tr>
+          <td class="custom-button" @click="routerChange('/play')">Play</td>
+          <td class="custom-button" @click="routerChange('/about')">About</td>
+        </tr>
+        <tr>
+          <td class="custom-button" @click="routerChange('/account')">Account</td>
+          <td class="custom-button" @click="logout()">Logout</td>
+        </tr>
+      </table>
+      <table v-else class="whole">
+        <tr>
+          <td class="custom-button" @click="routerChange('/donation?full=true')">Donate</td>
+          <td class="custom-button" @click="routerChange('/about')">About</td>
+        </tr>
+        <tr>
+          <td class="custom-button" @click="routerChange('/login')">Login</td>
+          <td class="custom-button" @click="routerChange('/register')">Register</td>
+        </tr>
+      </table>
+
+    </div>
+  </div>
 </template>
 
 <script>
-  import HomePage from "@/components/HomePage"
+  import {authTokenName} from '@/config/auth'
+  import localStorage from '@/helpers/localStorage'
 
   export default {
-    components: {
-        HomePage
+    methods: {
+      logout() {
+        localStorage.eraseCookie(authTokenName)
+        axios.defaults.headers['Authorization'] = localStorage.getCookie(authTokenName)
+        this.$forceUpdate()
+      },
+      isUserLoggedIn() {
+        return localStorage.isUserLoggedIn()
+      },
+      routerChange(url) {
+        this.$router.push(url)
+      }
     }
   }
 </script>
+
+<style scoped>
+  .whole {
+    height: 95%;
+    width: 100%;
+  }
+
+  .button-container {
+    height: 73%;
+    width: 100%;
+  }
+
+  .custom-button {
+    text-align: center;
+    border-style: solid;
+    border-width: 1px;
+    border-color: black;
+    width: 50%;
+    overflow-x: hidden;
+  }
+</style>
